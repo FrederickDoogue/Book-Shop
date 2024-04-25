8369f41bc066f67d2c58f24678758a8406810351
FrederickDoogue <frederickdoogue@hotmail.com>
2024-04-25 18:16:16 +0100

    MongoDB and mongoose connected

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root-fred:Waterfal13%3F@book-shop-mern.zgr3dts.mongodb.net/?retryWrites=true&w=majority&appName=Book-Shop-MERN';
